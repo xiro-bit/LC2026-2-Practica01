82746e02ab12e7b17f0b7ceb65986235b525b83d
xiro-bit <xirone@ciencias.unam.mx>
2026-02-19 21:11:32 -0600

    cambios en ejercicios restantes-practica completa, ya compila

--- README.md
+++ README.md
@@ -22,13 +22,12 @@ Si no les llegan a funcionar, es posible que el problema es que tengan una versi
 
 En esta sección deben eliminar esta línea de texto, borrar la leyenda "Integrante n" y escribir su nombre empezando por apellidos y su número de cuenta.
 
-+ Integrante 1
-    - No. de Cuenta: 
-+ Integrante 2
-    - No. de Cuenta: 
-+ Integrante 3
-    - No. de Cuenta: 
++ García Hernandez Greta Nicole
+     - No. de Cuenta: 322136313
++ Pérez García Blanca Xirone
+    - No. de Cuenta: 314213989
+
 
 ## Comentarios
 
-Recuerden que opcionalmente pueden agregar un comentario (ver los lineamientos de entrega). Si es el caso, borren esta línea y dejen su comentario.
+Se hicieron varias funciones auxiliares y constantes para poder realizar las implementaciones como las: de semiperimetro (el test me marco un warning porque no hicimos los demas casos y numero pi; palabraenCaracter y palabraEnReversa.
--- src/Practica01.hs
+++ src/Practica01.hs
@@ -8,11 +8,11 @@ data Shape = Circle Float | --representa el radio
             Rectangle Float Float| --representa base y altura
             Triangle Float| --representa cada uno un lado
             Trapeze Float Float Float --representa base mayor, base menor y altura
-            deriving (Show)
+            deriving (Show, Eq)
 
 --Funcion que calcula el area de las figuras
 
--- constante pi: definimos la constante pi para usarla en el caso del circuloare
+-- constante pi: definimos la constante pi para usarla en el caso del circulo
 numeropi :: Float
 numeropi = 3.1416
 
@@ -31,13 +31,14 @@ area (Trapeze x y z) = ((x + y)/2) * z
 semiperimetro :: Shape -> Float
 semiperimetro (Triangle x) = perimeter (Triangle x) /2
 
+
 --Funcion que calcula el perimetro de las figuras
 perimeter :: Shape -> Float
 perimeter (Square x) = x * 4
 perimeter (Rectangle x y) = (x + y) * 2 
 perimeter (Circle x) = (2 * x) * numeropi
 perimeter (Triangle x) = x * 3
-perimeter (Trapeze x y z) = ((x + y)/2) * z
+perimeter (Trapeze x y z) = x + y + 2* sqrt(z**2 +((x-y)/2)**2)
 
 
 
@@ -55,39 +56,68 @@ from0 (x,y)= sqrt(x**2 + y **2)
     
 
 --Ejercicio 3
-data Haskellium = Undefined
+data Haskellium = Haskellium {name :: String,
+                             lastName1 :: String,
+                             lastName2 :: String,
+                             houseShape :: Shape,
+                             location :: Point} 
+                             deriving (Show, Eq)
 
 --Funcion para regresar el hijo de dos Haskelliums dado su nombre
+--se asume que padre 1 y padre2 viven en la misma casa asi que la forma y la locacion es la misma
 son :: Haskellium -> Haskellium -> String -> Haskellium
-son = undefined
+son padre1 padre2 nombre = Haskellium {name = nombre, lastName1 = lastName1 padre1, lastName2 = lastName1 padre2, houseShape = houseShape padre1, location= location padre2}
 
 --Funcion para calcular las unidades para construir la casa de un Haskellium
 houseCost :: Haskellium -> Float
-houseCost = undefined
+houseCost h = area shape + perimeter shape * 2.5
+    where shape = houseShape h
+        
 
 --Funcion para calcular el tiempo que le toma a un Haskellium para llegar a su trabajo
 timeToWork :: Haskellium -> Float
-timeToWork = undefined
+timeToWork hask 
+    |distancia < 300 = distancia / 30  -- se ira en bici
+    |distancia >= 300  = distancia/ 70   -- se ira en moto
+    where distancia = from0 (location hask)
 
 --LISTAS Y FUNCIONES
 --Ejercicio 1
 palindromo :: String -> Bool
-palindromo = undefined
+palindromo palabra = lista == palabraEnReversa lista
+    where lista = palabraenCaracter palabra
+
+
+--funcion auxiliar que devuelve una lista de caracteres una palabra dada
+palabraenCaracter :: String -> [[Char]]
+palabraenCaracter word = [[c]| c <- word]
+
+--funcion auxiliar que devuelve la lista de caracteres al revés
+palabraEnReversa ::  [[Char]] -> [[Char]]
+palabraEnReversa []=[]
+palabraEnReversa (x:xs) = palabraEnReversa xs ++ [x]
+
+
 
 --Ejercicio 2
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = undefined
+myFoldr funcion acumulador []     = acumulador
+myFoldr funcion acumulador (x:xs) = funcion x (myFoldr funcion acumulador xs)
 
 --Ejercicio 3
 conjuntoPotencia :: [a] -> [[a]]
-conjuntoPotencia = undefined
+conjuntoPotencia [] = [[]]
+conjuntoPotencia (x:xs) = [x:ys| ys <-conjuntoPotencia xs] ++ conjuntoPotencia xs 
 
 --ARBOLES
 
 --Implementacion
 
-data OneTwoTree a = Undefinedd
+data OneTwoTree a = Void| Node a (OneTwoTree a)| Branch a (OneTwoTree a) (OneTwoTree a)
+
 
 --Ejercicio 2
 suma :: OneTwoTree Int -> Int
-suma = undefined
+suma Void = 0
+suma (Node x arbol) = x + suma arbol
+suma (Branch x arbol1 arbol2) = x + suma arbol1 + suma arbol2
